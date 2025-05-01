fa7290ea74686975e05ce017d7905ddab1eee067
sheng153 <nicolas.sabio@gmail.com>
2025-05-01 12:56:42 -0300

    Type: 📚 Docs
    Message: Documentacion autogenerada por doxygen

---
+++ documentation/backend/html/html/files_dup.js
@@ -0,0 +1,4 @@
+var files_dup =
+[
+    [ "lib.h", "lib_8h_source.html", null ]
+];
---
+++ documentation/backend/html/html/navtreeindex0.js
@@ -0,0 +1,8 @@
+var NAVTREEINDEX0 =
+{
+"files.html":[1,0],
+"index.html":[],
+"lib_8h_source.html":[1,0,0],
+"md_README.html":[0],
+"pages.html":[]
+};
